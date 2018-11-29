6b78930833bbb247c618a7c0bad44261f6830366
Luis Garcia <luis_grdz@hotmail.com>
2018-11-29 13:06:58 -0600

    cambios en el servicio de agregar casas 1.1

--- puertaapp/www/agregar.php
+++ puertaapp/www/agregar.php
@@ -1,79 +1,34 @@
 <?php //codigo cambios
-session_start();
 
-$titulo = "AGREGAR";
-
-include('include/header.php');
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $nocasa         = $_POST['Nocasa'];
-    $password       = $_POST['Password'];
-    $propietario    = $_POST['Propietario'];
-    $adeudo         = $_POST['Adeudo'];
+if (array_key_exists('numero', $_REQUEST)){
+    $nocasa         = $_REQUEST['numero'];
+    $password       = $_REQUEST['contra'];
+    $propietario    = $_REQUEST['nombre'];
+    $adeudo         = $_REQUEST['adeudo'];
     //script de agregar
-    $con=mysqli_connect("localhost","id127413_mario","mario","id127413_datos");
+
+    //conectamos la base de datos
+    $server="localhost";
+    $user="id6894656_mario";
+    $pass="mario";
+    $bd="id6894656_mario";
+
+    $conexion = new mysqli($server,$user,$pass,$bd);
+
+    //$con=mysqli_connect("localhost","id127413_mario","mario","id127413_datos");
     $sql="INSERT INTO fraccionamiento(NoCasa,Password,nombreP,Adeudo) 
     VALUES('$nocasa', '$password', '$propietario', '$adeudo')";
-    $retval=(mysqli_query($con,$sql));
+    $retval=(mysqli_query($conexion,$sql));
 
     if ($retval > 0) {
-        echo "<script>alert('Registro agregado con éxito');</script>";
+        //echo "<script>alert('Registro agregado con éxito');</script>";
+        echo "1";
     }else {
-        echo "<script>alert('Registro NO agregado');</script>";
+        //echo "<script>alert('Registro NO agregado');</script>";
+        echo "0";
     }
-    mysqli_close($con);
-    
+    mysqli_close($conexion);
+}else{
+    echo "0";
 }
 ?>
-<div class="container">
-<center>
-   <img src="https://craluminios.com.ar/wp-content/uploads/2017/04/d02-600x450.png" height=300 width=250>
-   <br>
-
-    <div class="row">
-        <div class="col s3">
-
-        </div>
-        
-        <div class="col s6">
-
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <h1>Agregar</h1>
-            <div class="input-field">
-                <input id="Nocasa" name="Nocasa" type="text" data-length="10" required>
-                <label for="Nocasa">No Casa</label>
-            </div>
-            <div class="input-field">
-                <input id="Password" name="Password" type="text" data-length="10" required>
-                <label for="Password">Password</label>
-            </div>
-            <div class="input-field">
-                <input id="Propietario" name="Propietario" type="text" data-length="10" required>
-                <label for="Propietario">Propietario</label>
-            </div>
-            <div class="input-field">
-                <input id="Adeudo" name="Adeudo" type="text" data-length="10" required>
-                <label for="Adeudo">Adeudo</label>
-            </div>
-            <div class="input-field">
-                <button class='btn waves-effect blue' type='submit'>AGREGAR
-                    <i class='material-icons right'>add_box</i>
-                </button>
-            </div>
-            <div class="input-field">
-                <a class='btn waves-effect red' href='principal.php'>ATRÁS
-                    <i class='material-icons right'>close</i>
-                </a>
-            </div>
-        </form>
-        </div>
-
-        </div>
-        
-        <div class="col s3">
-    </div>
-</center>
-</div>
-
-<?php include('include/footer.php'); ?>
